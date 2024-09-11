07c3b45d9e3d6f928b67523e0d388257f0f4f610
Catherine Zhao <91498917+cathzzr2@users.noreply.github.com>
2024-09-10 22:34:27 -0400

    Update log_record.go

--- data/log_record.go
+++ data/log_record.go
@@ -17,70 +17,64 @@ const (
 // 4 +  1  +  5   +   5 = 15
 const maxLogRecordHeaderSize = binary.MaxVarintLen32*2 + 5
 
-// LogRecord 写入到数据文件的记录
-// 之所以叫日志，是因为数据文件中的数据是追加写入的，类似日志的格式
+// LogRecord - writing into DataFile
+// Appended, log-like format
 type LogRecord struct {
 	Key   []byte
 	Value []byte
 	Type  LogRecordType
 }
 
-// LogRecord 的头部信息
+// LogRecord header info
 type logRecordHeader struct {
-	crc        uint32        // crc 校验值
-	recordType LogRecordType // 标识 LogRecord 的类型
-	keySize    uint32        // key 的长度
-	valueSize  uint32        // value 的长度
+	crc        uint32        // crc check value
+	recordType LogRecordType // LogRecord type
+	keySize    uint32        // key size
+	valueSize  uint32        // value size
 }
 
-// LogRecordPos 数据内存索引，主要是描述数据在磁盘上的位置
+// LogRecordPos data sorage index to describe the location on the disk
 type LogRecordPos struct {
-	Fid    uint32 // 文件 id，表示将数据存储到了哪个文件当中
-	Offset int64  // 偏移，表示将数据存储到了数据文件中的哪个位置
-	Size   uint32 // 标识数据在磁盘上的大小
+	Fid    uint32 // file id - which file data are stored
+	Offset int64  // offset - where data is stored in DataFile
+	Size   uint32 // data size on disk
 }
 
-// TransactionRecord 暂存的事务相关的数据
+// TransactionRecord 
 type TransactionRecord struct {
 	Record *LogRecord
 	Pos    *LogRecordPos
 }
 
-// EncodeLogRecord 对 LogRecord 进行编码，返回字节数组及长度
-//
-//	+-------------+-------------+-------------+--------------+-------------+--------------+
-//	| crc 校验值  |  type 类型   |    key size |   value size |      key    |      value   |
-//	+-------------+-------------+-------------+--------------+-------------+--------------+
-//	    4字节          1字节        变长（最大5）   变长（最大5）     变长           变长
+// EncodeLogRecord: encode LogRecord, return byte array and size
 func EncodeLogRecord(logRecord *LogRecord) ([]byte, int64) {
-	// 初始化一个 header 部分的字节数组
+	// intialize a byte array for header 
 	header := make([]byte, maxLogRecordHeaderSize)
-
-	// 第五个字节存储 Type
+	// store Type in 5th byte
 	header[4] = logRecord.Type
 	var index = 5
-	// 5 字节之后，存储的是 key 和 value 的长度信息
-	// 使用变长类型，节省空间
+	// after 5th byte, store size of key and value
+	// use binary.purVarint to save storage space
 	index += binary.PutVarint(header[index:], int64(len(logRecord.Key)))
 	index += binary.PutVarint(header[index:], int64(len(logRecord.Value)))
 
 	var size = index + len(logRecord.Key) + len(logRecord.Value)
 	encBytes := make([]byte, size)
 
-	// 将 header 部分的内容拷贝过来
+	// copy header 
 	copy(encBytes[:index], header[:index])
-	// 将 key 和 value 数据拷贝到字节数组中
+	// copy key 和 value to byte array
 	copy(encBytes[index:], logRecord.Key)
 	copy(encBytes[index+len(logRecord.Key):], logRecord.Value)
 
-	// 对整个 LogRecord 的数据进行 crc 校验
+	// check LogRecord with crc 
 	crc := crc32.ChecksumIEEE(encBytes[4:])
 	binary.LittleEndian.PutUint32(encBytes[:4], crc)
 
 	return encBytes, int64(size)
 }
 
-// EncodeLogRecordPos 对位置信息进行编码
+// EncodeLogRecordPosition
 func EncodeLogRecordPos(pos *LogRecordPos) []byte {
 	buf := make([]byte, binary.MaxVarintLen32*2+binary.MaxVarintLen64)
 	var index = 0
@@ -90,10 +84,10 @@ func EncodeLogRecordPos(pos *LogRecordPos) []byte {
 	return buf[:index]
 }
 
-// DecodeLogRecordPos 解码 LogRecordPos
+// DecodeLogRecordPosition
 func DecodeLogRecordPos(buf []byte) *LogRecordPos {
 	var index = 0
-	fileId, n := binary.Varint(buf[index:]) //buf是有三个元素的数组
+	fileId, n := binary.Varint(buf[index:]) //buf: array with 3 elements
 	index += n
 	offset, n := binary.Varint(buf[index:])
 	index += n
@@ -101,7 +95,7 @@ func DecodeLogRecordPos(buf []byte) *LogRecordPos {
 	return &LogRecordPos{Fid: uint32(fileId), Offset: offset, Size: uint32(size)}
 }
 
-// 对字节数组中的 Header 信息进行解码
+// encode Header in byte array
 func decodeLogRecordHeader(buf []byte) (*logRecordHeader, int64) {
 	if len(buf) <= 4 {
 		return nil, 0
@@ -113,12 +107,12 @@ func decodeLogRecordHeader(buf []byte) (*logRecordHeader, int64) {
 	}
 
 	var index = 5
-	// 取出实际的 key size
+	// get actual key size
 	keySize, n := binary.Varint(buf[index:])
 	header.keySize = uint32(keySize)
 	index += n
 
-	// 取出实际的 value size
+	// get actual value size
 	valueSize, n := binary.Varint(buf[index:])
 	header.valueSize = uint32(valueSize)
 	index += n
